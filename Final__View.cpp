edfa4ecda822fc6b42b3276a40246f3a61542f6a
유림 이 <bu2990@bu.ac.kr>
2020-12-15 09:26:01 +0900

    버전3

--- Final__View.cpp
+++ Final__View.cpp
@@ -20,7 +20,7 @@
 #define UNKNOWN 0
 #define Butterfly 1
 #define Child 2
-#define Move 3
+#define Move 3 
 
 // CFinalView
 
@@ -48,7 +48,7 @@ CFinalView::CFinalView() noexcept
 
 	m_kind = UNKNOWN;
 	count = 0;
-	m_index = 1;
+	m_index = 1;//사진이 1부터이므로 1로 초기화함
 	m_xPos = 0;
 }
 
@@ -72,7 +72,7 @@ void CFinalView::OnDraw(CDC* pDC)
 	ASSERT_VALID(pDoc);
 	if (!pDoc)
 		return;
-
+	//첫 페이지 
 	CRect rt(100, 100, 300, 200);
 	pDC->DrawText(L"20162990 이유림 \n 기말고사 프로젝트", &rt, DT_CENTER | DT_VCENTER);
 	CRect rt2(400, 300, 1000, 600);
@@ -95,12 +95,14 @@ void CFinalView::OnDraw(CDC* pDC)
 
 	}
 	else if (m_kind == Child) {
+		CRect message(50, 0, 800, 200);
+		pDC->DrawText(L"마우스를 한번 누르면 아이가 공원에서 뛰어다니는 모습이 나옵니다^^", &message, DT_CENTER | DT_VCENTER);
 		CBitmap forest;
 		forest.LoadBitmapW(IDB_BIT_FOREST);
 		CDC mdc;
 		mdc.CreateCompatibleDC(pDC);
 		mdc.SelectObject(&forest);
-		pDC->BitBlt(0, 0, 1200, 700, &mdc, 0, 0, SRCCOPY);//배경 숲
+		pDC->BitBlt(0, 50, 1200, 700, &mdc, 0, 0, SRCCOPY);//배경 숲
 
 		CString filename;
 		CImage child;
@@ -110,13 +112,16 @@ void CFinalView::OnDraw(CDC* pDC)
 	}
 
 		else if (m_kind == Move) {
+			CRect message(50, 0, 1000, 200);
+			pDC->DrawText(L"마우스를 한번 누르면 아이는 뛰어다니고 나비는 날아다니는 공원 모습이 나타납니다^^", &message, DT_CENTER | DT_VCENTER);
+
 			CBitmap forest;
 			forest.LoadBitmapW(IDB_BIT_FOREST);
 			CDC mdc;
 			mdc.CreateCompatibleDC(pDC);
 			mdc.SelectObject(&forest);
-			pDC->BitBlt(0 - count, 0, 1200, 700, &mdc, 0, 0, SRCCOPY);
-			pDC->BitBlt(1200 - count, 0, 1200, 700, &mdc, 0, 0, SRCCOPY);
+			pDC->BitBlt(0 - count, 50, 1200, 700, &mdc, 0, 0, SRCCOPY);
+			pDC->BitBlt(1200 - count, 50, 1200, 700, &mdc, 0, 0, SRCCOPY);
 			//배경 숲이 이동하는 것처럼 보이게 만듦
 			CImage butterfly;
 			butterfly.Load(L"res/butterfly.png");
@@ -127,14 +132,6 @@ void CFinalView::OnDraw(CDC* pDC)
 			filename.Format(L"res/child(%d).png", m_index);
 			child.Load(filename);
 			child.Draw(*pDC, m_xPos, 200, 100, 200);
-			/*CBitmap boy, * oldbit;
-			boy.LoadBitmapW(IDB_BIT_CHILD);
-			CDC memdc;
-			memdc.CreateCompatibleDC(pDC);
-			memdc.SelectObject(&boy);
-			oldbit = memdc.SelectObject(&boy);
-			pDC->BitBlt(m_Child.x, m_Child.y, 100, 200, &memdc, m_count * 100, 0, SRCCOPY);
-			memdc.SelectObject(oldbit);*/
 		}
 	}
 	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
@@ -198,7 +195,7 @@ void CFinalView::OnLButtonDown(UINT nFlags, CPoint point)
 		SetTimer(0, 100, NULL);
 	}
 	else if (m_kind == Move) {
-		SetTimer(1, 100, NULL);
+		SetTimer(1, 150, NULL);
 		m_xStep = (int)(point.x - m_pt.x) / 10;
 		m_yStep = (int)(point.y - m_pt.y) / 10;
 		m_count = 0;
@@ -216,13 +213,13 @@ void CFinalView::OnTimer(UINT_PTR nIDEvent)
 
 		m_pt.x = m_pt.x + m_xStep;
 		m_pt.y = m_pt.y + m_yStep;
-		if ((m_pt.x - 50) < 0 || (m_pt.x + 50) > 1200) //숲 배경에 맞춰서 돌아다니게 함.
+		if ((m_pt.x - 50) < 0 || (m_pt.x + 50) > 1200) //숲 배경에 맞춰서 돌아다니게 함(x축).
 		{
 			m_pt.x = (m_xStep < 0) ? 50 : (1200 - 50);
 			m_pt.y = m_pt.y - m_yStep;
 			m_xStep = m_xStep * -1;
 		}
-		if ((m_pt.y - 50) < 0 || (m_pt.y + 50) > 750) // 숲 배경에 맞춰서 돌아다니게 함.
+		if ((m_pt.y - 50) < 0 || (m_pt.y + 50) > 750) // 숲 배경에 맞춰서 돌아다니게 함(y축).
 		{
 			m_pt.x = m_pt.x - m_xStep;
 			m_pt.y = (m_yStep < 0) ? 50 : 750 - 50;
@@ -236,28 +233,28 @@ void CFinalView::OnTimer(UINT_PTR nIDEvent)
 		if (m_index == 7)
 			m_index = 1;
 		m_xPos = m_xPos + 10;
-		Invalidate();
+		Invalidate(0);//깜빡임이 덜하도록 0을 넣어준다.
 	}
 	else if (m_kind == Move) {//메뉴에서 Move 누르면 호출
 		m_pt.x = m_pt.x + m_xStep;
 		m_pt.y = m_pt.y + m_yStep;
-		if ((m_pt.x - 50) < 0 || (m_pt.x + 50) > m_WinRight)
+		if ((m_pt.x - 50) < 0 || (m_pt.x + 50) > m_WinRight)//Butterfly 와는 다르게 윈도우창 좌우 끝까지 움직일수 있다.
 		{
 			m_pt.x = (m_xStep < 0) ? 50 : (m_WinRight - 50);
 			m_pt.y = m_pt.y - m_yStep;
 			m_xStep = m_xStep * -1;
 		}
-		if ((m_pt.y - 50) < 0 || (m_pt.y + 50) > m_WinBottom)
+		if ((m_pt.y - 50) < 0 || (m_pt.y + 50) > 750)
 		{
 			m_pt.x = m_pt.x - m_xStep;
-			m_pt.y = (m_yStep < 0) ? 50 : (m_WinBottom - 50);
+			m_pt.y = (m_yStep < 0) ? 50 : (750 - 50);
 			m_yStep = m_yStep * -1;
 		}
 		m_index = m_index + 1;
 		if (m_index == 7)
 			m_index = 1;
 		m_xPos = m_xPos + 10;
-		Invalidate();
+		
 		m_count++;
 		if (m_count == 6)
 			m_count = 0;
@@ -265,10 +262,9 @@ void CFinalView::OnTimer(UINT_PTR nIDEvent)
 		Invalidate();
 		if (count == 1200) {
 			count = 0;
-			Invalidate();
-
+		
 		}
-
+		Invalidate(0);
 	}
 }
 
@@ -282,7 +278,7 @@ void CFinalView::OnSize(UINT nType, int cx, int cy)
 }
 
 
-void CFinalView::OnPlayButterfly()
+void CFinalView::OnPlayButterfly()//나비가 날아다니는 모습
 {
 	// TODO: 여기에 명령 처리기 코드를 추가합니다.
 	m_kind = Butterfly;
@@ -290,7 +286,7 @@ void CFinalView::OnPlayButterfly()
 }
 
 
-void CFinalView::OnPlayChild()
+void CFinalView::OnPlayChild()//아이가 뛰어다니는 모습
 {
 	// TODO: 여기에 명령 처리기 코드를 추가합니다.
 	m_kind = Child;
@@ -298,10 +294,11 @@ void CFinalView::OnPlayChild()
 }
 
 
-void CFinalView::OnParkMove()
+void CFinalView::OnParkMove()//공원에서 아이가 뛰어다니고 나비가 날아다니는 모습
 {
 	// TODO: 여기에 명령 처리기 코드를 추가합니다.
 	m_kind = Move;
+	Invalidate();
 }
 
 
